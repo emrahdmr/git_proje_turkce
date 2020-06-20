78c0c4356e3fb15fe21d9ab04e922c2135cfc3a8
emrahdmr <bakiemrah@hotmail.com>
2020-06-20 22:49:42 +0300

    son guncel

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -8,13 +8,8 @@ public class FirstClass {
 		System.out.println("first commit from local repository");
         
 		System.out.println("second commit from local repository");
-<<<<<<< HEAD
-		System.out.println("ikinci kez conflict yapiyoruz");
-        System.out.println("first commit from remote repository");
-=======
 		System.out.println("we are against the conflict");
                 System.out.println("first commit from remote repository");
->>>>>>> 1caecbd87ab0d1368a84052750d3260f2b25760f
 		
 	}
 
